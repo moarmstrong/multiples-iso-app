a39c17f950fd7df01af7be0692fe6d136f8ddbe5
moarmstrong <themoarmstrong@gmail.com>
2016-04-27 14:47:53 -0400

    Added Variables/Outlets/Actions

--- multiples-app/ViewController.swift
+++ multiples-app/ViewController.swift
@@ -9,16 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+// Variables
+    var maxMultiple = 0
+    var currentMultiple = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+// Outlets
+    @IBOutlet weak var multLogo: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var multipleTxt: UITextField!
+    
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var multipleLbl: UILabel!
+    
+// Actions
+    @IBAction func onPlayButtonPressed(sender: UIButton!) {
+       
+        
+        if multipleTxt.text != nil && multipleTxt.text != "" { // Validation
+            
+            multLogo.hidden = true
+            playBtn.hidden = true
+            multipleTxt.hidden = true
+            
+            addBtn.hidden = false
+            multipleLbl.hidden = false
+            
+            
+            maxMultiple = Int(multipleTxt.text!)! // Validation
+            currentMultiple = 0
+            
+            multipleLbl.text = "\(currentMultiple) + \(maxMultiple) = \(currentMultiple)";
+            currentMultiple += maxMultiple;
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
